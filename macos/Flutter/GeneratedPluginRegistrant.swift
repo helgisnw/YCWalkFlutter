3bcbcd5a5fb0bf797360cf8ba043e68691d0b361
JIN WOOHYUN <helgisnw@gmail.com>
2023-10-20 12:13:19 +0900

    edit

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,7 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus
+import device_info_plus_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
